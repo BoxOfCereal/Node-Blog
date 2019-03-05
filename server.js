1986406dfe8b7889f4de934be357b10aa2c6508c
Noah <noah.stoeckel@gmail.com>
2019-03-05 18:57:25 -0500

    Sets Up A Skeleton Of The Project
    - Adds users routes
    - Adds posts routes
    - Sets up basic middleware inside server.js

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const server = express();
+server.use(express.json());
+server.use(morgan());
+server.use(helmet());
+server.use(cors());
+
+const userRoutes = require("./routes/users");
+server.use("/api/users", userRoutes);
+
+const postsRoutes = require("./routes/posts");
+server.use("/api/posts", postsRoutes);
 
 module.exports = server;
