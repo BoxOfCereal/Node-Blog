c6fe84d044e9994c5d0875b97395826158768d01
Noah <noah.stoeckel@gmail.com>
2019-03-05 19:50:19 -0500

    Adds An Endpoint To Get All The Posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../data/helpers/postDb");
+
+router.get("/", async (req, res) => {
+  try {
+    const posts = await db.get();
+    res.status(200).json(posts);
+  } catch (e) {
+    res.status(500).json({ error: "Could Not Retrieve Posts" });
+  }
+});
 
 module.exports = router;
